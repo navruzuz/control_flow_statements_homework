4bfce06d5fdd530cb3f590fcf1d24e1916490525
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-15 22:14:46 +0500

    ok

--- if02.py
+++ if02.py
@@ -1,9 +1,15 @@
 def main(a):
-    """
-    If the number is positive, increase it to 1,otherwise decrease it to 2.
-    Args:
-        a: integer
-    Returns:
-        a: a increased by 1 if positive, else decreased by 2.
-    """
-    return
+   
+    if a>0:
+        print('positive')
+        if a>0:
+            print(a+1)
+        else:
+            print(a-2)
+    else:
+        print('negative') 
+        if a>0:
+            print(a+1)  
+        else:
+            print(a-2)     
+print(main(-5))
