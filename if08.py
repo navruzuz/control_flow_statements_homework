4d89903734d8dfab8adc3188f363ad263bdd3004
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-19 04:45:23 +0500

    bajarildi

--- if08.py
+++ if08.py
@@ -1,14 +1,32 @@
 def main(a):
-    """
-    Given an integer a, check the following conditions:
-    "two-digit odd number",
-    "two-digit even number",
-    "three-digit odd number",
-    "three-digit even number"
-
-    Args:
-        a: integer
-    Returns:
-        string: the message to print
-    """
-    return
+    if a%2==0:
+        print('a juft son')
+        if (a%10)%2==0 and a>10 and a<100 :
+            print('a ikki xonali juft son')
+        if (a%10)%2!=0 and a>10 and a<100 :
+            print('a ikki xonali toq son')
+        if (a%100)%2==0 and a>100 and a<1000:
+            print('a uch xonali juft son')
+        if (a%100)%2!=0 and a>100 and a<1000:
+            print('a uch xonali toq son')
+    else:
+        print('a toq son')
+        if (a%10)%2==0 and a>10 and a<100 :
+            print('a ikki xonali juft son')
+        if (a%10)%2!=0 and a>10 and a<100 :
+            print('a ikki xonali toq son')
+        if (a%100)%2==0 and a>100 and a<1000:
+            print('a uch xonali juft son')
+        if (a%100)%2!=0 and a>100 and a<1000:
+            print('a uch xonali toq son')
+        
+        # if (a%10)%2==0 :
+        #     print('a ikki xonali juft son')
+        # if (a%100)%2==0:
+        #     print('a uch xonali juft son')
+        # if (a%10)%2!=0 :
+        #     print('a ikki xonali toq son')
+        # if (a%100)%2!=0 :
+        #     print('a uch xonali toq son')
+print(main(321))
+    
