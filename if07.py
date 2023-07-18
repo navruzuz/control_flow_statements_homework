e3e855c69ea9d24e6509e6441a23f9c0982ba5a7
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-18 11:23:30 +0500

    bajarildi

--- if07.py
+++ if07.py
@@ -1,15 +1,16 @@
 def main(a):
-    """
-    Given an integer a, check the following conditions:
-    "positive odd number",
-    "positive even number",
-    "negative odd number",
-    "negative even number",
-    "the number is zero"
-
-    Args:
-        a: integer
-    Returns:
-        string: the message to print
-    """
-    return
+    if a>0:
+        print('a musbat son')
+        if a%2==0:
+            print('a musbat juft son')
+        else:
+            print('a musbat toq son')
+    if a==0:
+        return 0
+    else:
+        print('a manfiy son')
+        if a%2==0:
+            print('a manfiy juft son')
+        else:
+            print('a manfiy toq son')
+print(main(-1))
