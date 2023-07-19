8c87babbb1f11e809236c4e4218b39a8f4b34971
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-19 05:47:29 +0500

    bajarildi

--- if05.py
+++ if05.py
@@ -1,11 +1,27 @@
 def main(a,b,c):
-    """
-    Find number of negative numbers there are in the given numbers.
-    Args:
-        a: integer
-        b: integer
-        c: integer
-    returns:
-        integer: the number of negative numbers in the given numbers
-    """
-    return
+    if a<0 and b<0 and c<0:
+        print('3ta manfiy son')
+    if (a<0 and b<0) or (c<0 and b<0) or (a<0 and c<0):
+        print('2ta manfiy son')
+    if (a<0) or (b<0) or (c<0):
+        print('1ta manfiy son')
+    if (a==0) or (b==0) or (c==0) :
+        return 0
+print(main(3,-8,0))
+    
+    # if a<0 and b<0 and c>0:
+    #     print('2ta manfiy son')
+    # if a<0 and b>0 and c<0:
+    #     print('2ta manfiy son')
+    # if a>0 and b<0 and c<0:
+    #     print('2ta manfiy son')
+    # if a>0 and b>0 and c>0:
+    #     print(' manfiy son mavjud emas')
+    # if a<0 and b<0 and c<0:
+    #     print('3ta manfiy son')
+    # if a<0 and b<0 and c<0:
+    #     print('3ta manfiy son')
+    # if a<0 and b<0 and c<0:
+    #     print('3ta manfiy son')
+    
+        
